fa6dcbb14336434350fac470638cc978444b1f65
m-pilia <martino.pilia@gmail.com>
2016-01-19 15:19:43 +0100

    check if specific popup is open

--- js/popup.js
+++ js/popup.js
@@ -5,6 +5,12 @@
  * @date 2016-01-18
  */
 
+"use strict";
+
+/**
+ * Open the popup with the specified id.
+ * @param  {string} popupName The id for the popup div.
+ */
 function openPopup(popupName) {
     $('#' + popupName).addClass('popup-visible');
     $('.body-wrapper').addClass('overlay').click(function () {
@@ -12,7 +18,25 @@ function openPopup(popupName) {
     });
 }
 
+/**
+ * Close any open popup.
+ */
 function closePopup() {
     $('.popup-visible').removeClass('popup-visible');
     $('.overlay').removeClass('overlay');
+}
+
+/**
+ * Check if a popup is open.
+ * @param {string} name Id for the popup. If not specified, the check is
+ *                      done for any popup.
+ * @return {Boolean} True if the specified popup (or any popup, if
+ *                   not specified) is open.
+ */
+function isPopupOpen(name) {
+    if (name === undefined || name === null || $.type(name) !== "string")
+        name = '';
+    else
+        name = "#" + name;
+    return $(name + '.popup-visible').length > 0;
 }
